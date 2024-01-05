a62edcb71934961b17ae2edae9cb822fc0120324
Aman Kumar Jha <testbotharsh@gmail.com>
2024-01-05 21:14:28 +0000

    string number conversion

---
+++ 01_basics/string-number-conversion.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+console.log(typeof score)
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(booleanLoggedIn)
+
+//"" => false; "aman" => true; 1 => true; 0=> false
