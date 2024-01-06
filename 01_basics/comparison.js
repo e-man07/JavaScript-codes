424b19b51e52a23db703d9cc1d6b4a92116a68af
Aman Kumar Jha <testbotharsh@gmail.com>
2024-01-06 19:43:11 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,16 @@
+// => Avoid these type of comparisons
+
+// console.log("2" > 1)
+// console.log("02" >1)
+
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null > 0)
+
+ //console.log(undefined == 0)
+ //console.log(undefined >= 0)
+ //console.log(undefined > 0)
+
+ // === (strictly check)
+
+ console.log("2" === 2)
