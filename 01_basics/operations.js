0583df345ffb100aa897c4cb2ca96cf4a035ba54
Aman Kumar Jha <testbotharsh@gmail.com>
2024-01-06 19:26:10 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,14 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue)
+
+
+// string to number conversion and vice versa
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2+ "2")
+
+console.log(+true)
+console.log(+"")
